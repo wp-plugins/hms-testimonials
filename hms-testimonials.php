363c2b104a08b38e1dfc94df8a2bd909b5e58936
kreitje <kreitje@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-08 21:01:11 +0000

    tagging version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/hms-testimonials/tags/1.4@665391 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hms-testimonials.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: HMS Testimonials
+Plugin URI: http://hitmyserver.com
+Description: Displays your customer testimonials.
+Version: 1.4
+Author: HitMyServer LLC
+Author URI: http://hitmyserver.com
+*/
+
+
+define('HMS_TESTIMONIALS', plugin_dir_path(__FILE__));
+require_once HMS_TESTIMONIALS . 'setup.php';
+require_once HMS_TESTIMONIALS . 'shortcodes.php';
+require_once HMS_TESTIMONIALS . 'widgets.php';
+require_once HMS_TESTIMONIALS . 'admin.php';
+
+$hms_testimonials_db_version = 7;
+
+
+
+add_action('wp_enqueue_scripts', create_function('', 'wp_enqueue_script(\'jquery\');'));
+add_action('plugins_loaded', 'hms_testimonials_db_check');
+
+add_action('admin_init', create_function('', 'HMS_Testimonials::getInstance();'));
+add_action('admin_menu', create_function('', 'HMS_Testimonials::getInstance()->admin_menus();'));
+
+add_action('widgets_init', 'hms_testimonials_widgets');
+
+add_shortcode('hms_testimonials', 'hms_testimonials_show');
+add_shortcode('hms_testimonials_rotating', 'hms_testimonials_show_rotating');
+
+
+add_filter('plugin_action_links', array(HMS_Testimonials, 'settings_link'), 10, 2);
